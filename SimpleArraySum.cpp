211bc60e027a22927ca7af3b5dd19584c120d294
SasiDharKM <sasidhar.km97@gmail.com>
2017-04-02 00:02:34 +0530

    Added the solution to the second problem in warmup series

---
+++ SimpleArraySum.cpp
@@ -0,0 +1,57 @@
+/*Given an array of  integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. 
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+*/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n,sum=0;
+    cin >> n;
+   vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    cout<<sum;
+    return 0;
+}

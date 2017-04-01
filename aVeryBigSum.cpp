0716d6e341c1142760e66d4226acbba423a9e676
SasiDharKM <sasidhar.km97@gmail.com>
2017-04-02 00:09:58 +0530

    Added the solution to the fourth problem in warmup series

---
+++ aVeryBigSum.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+
+int main(){
+    int n;
+    long int sum=0;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    cout<<sum;
+    return 0;
+}

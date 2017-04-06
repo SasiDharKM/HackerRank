c33efaf3bc77813a9701e368dba6b3c4a0bd4c78
SasiDharKM <sasidhar.km97@gmail.com>
2017-04-06 11:49:36 +0530

    Added a solution

---
+++ beautiful_binary_string.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+B = raw_input().strip()
+i=-3
+count=0
+while True:
+    i = B.find('010',i+3)
+    if i != -1:
+        count+=1
+    else:
+        break
+print count        

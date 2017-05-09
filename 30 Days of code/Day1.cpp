22052fbf43a93b96f0321a0350aceac4fddfd6b7
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-09 11:37:07 +0530

    Added a solution

---
+++ 30 Days of code/Day1.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+int main() 
+{
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    int a;
+    double b;
+    string c;
+    cin>>a>>b;
+    getline(cin>>ws,c);
+    
+    cout<<i+a;
+    cout<<'\n'<<fixed<<setprecision(1)<<d+b;
+    cout<<'\n'<<s<<c;
+    return 0;
+
+}

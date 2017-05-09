6e9cf309b4d084d660fd8f9d5cb2b9a2f8e74830
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-09 11:32:06 +0530

    Added a solution and some restructuring

---
+++ 30 Days of code/Day2.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    double base;
+    int tip,tax;
+    cin>>base>>tip>>tax;
+    cout<<"The total meal cost is "<<int(base+(base*tip/100)+(base*tax/100))<<" dollars.";
+    return 0;
+}

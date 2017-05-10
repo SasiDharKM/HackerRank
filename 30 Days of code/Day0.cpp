90a28883b528eaed6cf16848c64eaa6d9932d6eb
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-10 19:14:15 +0530

    just found subl3 does not support stdin so some tinkering

--- 30 Days of code/Day0.cpp
+++ 30 Days of code/Day0.cpp
@@ -1,8 +1,19 @@
+/*input
+Sasidhar
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int main() {
     
     string input_string;   
-    getline(cin, input_string);  
-    cout << "Hello, World." << endl;
-    cout << input_string;
+    cout<<"enter a string\n";
+    cin>>input_string;
+    cout <<"Hi and " <<"Hel, World." << endl;
+    cout << input_string<<'\n';
     return 0;
 }

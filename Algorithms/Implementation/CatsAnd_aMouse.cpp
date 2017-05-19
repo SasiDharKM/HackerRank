d90e0408748f62776fe07ada96b8bd0f67bd94aa
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-19 11:51:26 +0530

    Added a solution

---
+++ Algorithms/Implementation/CatsAnd_aMouse.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+
+int main(){
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int x;
+        int y;
+        int z;
+        cin >> x >> y >> z;
+        if(abs(x-z)<abs(y-z))
+            cout<<"Cat A"<<endl;
+        else if(abs(x-z)>abs(y-z))
+            cout<<"Cat B"<<endl;
+        else 
+            cout<<"Mouse C"<<endl;
+    }
+    return 0;
+}

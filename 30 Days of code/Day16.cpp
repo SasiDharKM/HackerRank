31dcea479ddd6032dd30a3dcc61588b6df2bb348
SasiDharKM <sasidhar.km97@gmail.com>
2017-07-17 05:55:52 +0530

    Added a Solution

--- 30 Days of code/Day16.cpp
+++ 30 Days of code/Day16.cpp
@@ -1,7 +1,13 @@
+
+
+using namespace std;
+#include <bits/stdc++.h>
 using namespace std;
 
-
 int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
     string S;
     cin >> S;
     try{

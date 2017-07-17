855185ed6a737db88abeff132196aeff27e4de69
SasiDharKM <sasidhar.km97@gmail.com>
2017-07-17 05:40:41 +0530

    Added a Solution

--- Algorithms/Strings/beautiful_binary_string.py
+++ Algorithms/Strings/beautiful_binary_string.py
@@ -8,9 +8,9 @@ B = raw_input().strip()
 i=-3
 count=0
 while True:
-    i = B.find('010',i+3)
+    i = B.find('010',i+1)
     if i != -1:
         count+=1
     else:
         break
-print count        
+print (count)        
